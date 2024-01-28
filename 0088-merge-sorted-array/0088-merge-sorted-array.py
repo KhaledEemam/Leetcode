630baeae27ffeaaf71f8ae63585476b8149b8110
KhaledEemam <khaledeemam@gmail.com>
2024-01-28 16:58:03 +0200

    Time: 37 ms (84.56%), Space: 16.6 MB (64.64%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.py
+++ 0088-merge-sorted-array/0088-merge-sorted-array.py
@@ -1,15 +1,38 @@
-class Solution(object):
-    def merge(self, nums1, m, nums2, n):
-        if n == 0:
-            first_array = nums1[:]
-        else:
-            first_array = nums1[:-n]
-            
-        final = first_array + nums2
+class Solution:
+    def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
+        """
+        Do not return anything, modify nums1 in-place instead.
+        """
+        nums1_divided = nums1[:m]
         
-        for i in range(n) :
-            nums1[m+i] = final[m+i]
-            
-        nums1.sort()
+        i , k , h = 0 , 0 , 0 
         
+        while i < m and k < n :
+            if nums1_divided[i] < nums2[k] :
+                nums1[h] = nums1_divided[i]
+                i += 1
+                h += 1
+            elif nums1_divided[i] > nums2[k] :
+                nums1[h] = nums2[k]
+                k += 1
+                h += 1
+            else :
+                nums1[h] , nums1[h+1] = nums1_divided[i] , nums2[k] 
+                h += 2
+                k+= 1
+                i += 1
+                
+                
+                
+        if i == m :
+            nums1[h:] = nums2[k:]
+        elif k == n :
+            nums1[h:] = nums1_divided[i:]
+            
+                
+                
+
+        
+                
+                
         
