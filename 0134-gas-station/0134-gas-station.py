73e8b4bcb9f45d338419831529e0670e9452837b
KhaledEemam <khaledeemam@gmail.com>
2024-01-31 22:38:05 +0200

    Time: 925 ms (92.50%), Space: 21.9 MB (97.44%) - LeetHub

--- 0134-gas-station/0134-gas-station.py
+++ 0134-gas-station/0134-gas-station.py
@@ -3,6 +3,16 @@ class Solution:
         if sum(cost) > sum(gas) :
             return -1
         
+        total = 0 
+        start = 0 
+        for i in range(len(gas)) :
+            total += gas[i] - cost[i]
+            if total < 0 :
+                total = 0 
+                start = i + 1
+        
+        return start
+    """ 
         start = 0
         total = 0
         
@@ -13,7 +23,9 @@ class Solution:
                 start = i+1
                 
         return start
-        """ 
+    
+    """
+    """ 
         def check(i) :
             cur_station = i
             gas_amount = gas[i]
