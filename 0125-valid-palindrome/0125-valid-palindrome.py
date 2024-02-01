aba8c842287b891ddd222c2aebd3a286a1559853
KhaledEemam <khaledeemam@gmail.com>
2024-02-02 00:43:54 +0200

    Time: 42 ms (81.08%), Space: 17 MB (97.93%) - LeetHub

---
+++ 0125-valid-palindrome/0125-valid-palindrome.py
@@ -0,0 +1,27 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        text = ''
+        for letter in s :
+            if letter.isalpha()  :
+                text += (letter.lower())
+            elif letter.isdigit() :
+                text += letter
+                
+        l , r = 0 , len(text)-1
+        
+        if l == r :
+            return True
+        
+        while l < r :
+            if text[l] == text[r] :
+                l += 1 
+                r -= 1
+            else :
+                return False
+            
+        return True
+                
+            
+            
+                
+        
