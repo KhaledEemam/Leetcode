8358c88f52d59c1d718ddef69d8ead35ad218e6b
KhaledEemam <khaledeemam@gmail.com>
2024-02-03 00:30:14 +0200

    Time: 50 ms (79.77%), Space: 16.6 MB (71.69%) - LeetHub

--- 0080-remove-duplicates-from-sorted-array-ii/0080-remove-duplicates-from-sorted-array-ii.py
+++ 0080-remove-duplicates-from-sorted-array-ii/0080-remove-duplicates-from-sorted-array-ii.py
@@ -1,5 +1,38 @@
 class Solution:
     def removeDuplicates(self, nums: List[int]) -> int:
+        j = 1
+        count = 1
+        
+        for i in range(1,len(nums)) :
+            if (nums[i] == nums[j-1]) and count < 2 :
+                nums[j] = nums[i]
+                j += 1
+                count += 1
+            elif nums[i] != nums[j-1] :
+                nums[j] = nums[i]
+                count = 1
+                j += 1
+                
+        return j
+                
+                
+        """
+        l , r = 0 , 0
+        while r < len(nums) :
+            count = 1 
+            while (((r+1) < len(nums)and(nums[r] == nums[r+1]))) :
+                count += 1 
+                r += 1 
+            
+            for i in range(min(2,count)) :
+                nums[l] = nums[r]
+                l += 1
+            
+            r+= 1
+            
+        return l      
+                
+   #################################################################################     
         j = 0 
         my_hash = {}
         
@@ -14,5 +47,4 @@ class Solution:
                 j += 1
                 
         return j
-                
-                
+           """         
