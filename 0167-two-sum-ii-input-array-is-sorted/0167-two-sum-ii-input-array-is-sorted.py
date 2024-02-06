d5c86544da506d1769475f5944584c77d8bfe3e5
KhaledEemam <khaledeemam@gmail.com>
2024-02-06 03:11:45 +0200

    Time: 85 ms (99.87%), Space: 17.6 MB (55.04%) - LeetHub

--- 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.py
+++ 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.py
@@ -1,5 +1,19 @@
 class Solution:
     def twoSum(self, numbers: List[int], target: int) -> List[int]:
+        numbers.sort()
+        l , r = 0 , len(numbers) -1
+        
+        while r > l :
+            two_sum = numbers[l] + numbers[r]
+            if two_sum > target :
+                r -= 1
+            elif two_sum < target :
+                l += 1
+            else :
+                return [l+1,r+1]
+                
+        
+        """
         my_hash = {}
         
         for i in range(len(numbers)) :
@@ -8,4 +22,4 @@ class Solution:
                 return [my_hash[diff]+1,i+1]
             else :
                 my_hash[numbers[i]] = i
-        
+        """
