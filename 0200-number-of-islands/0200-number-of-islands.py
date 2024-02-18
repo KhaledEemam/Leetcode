ead5235aca6f5e162ec1d50333529708563bf6d5
KhaledEemam <khaledeemam@gmail.com>
2024-02-18 03:20:54 +0200

    Time: 260 ms (47.47%), Space: 27.8 MB (12.47%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.py
+++ 0200-number-of-islands/0200-number-of-islands.py
@@ -1,34 +1,31 @@
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
-        if not grid :
-            return 0
-        
         rows , columns = len(grid) , len(grid[0])
-        visit = set()
-        isalnds = 0
+        visited = set()
         
-        def bfs(r,c) :
-            q = collections.deque()
-            visit.add((r,c))
-            q.append((r,c))
+        def dfs(r,c) :
+            if r < 0 or c < 0 or r== rows or c == columns or grid[r][c] == "0"or (r,c) in visited :
+                return False
             
-            while q :
-                row,column = q.popleft()
-                directions = [[1,0],[-1,0],[0,1],[0,-1]]
-
-                for dr , dc in directions :
-                    r , c = row+dr , column+dc
-                    if ((r,c) not in visit and r in range(rows) and c in range(columns) and grid[r][c] == "1")  :
-                        q.append((r,c))
-                        visit.add((r,c))
+            visited.add((r,c))
             
-        for r in range(rows) :
-            for c in range(columns) :
-                if (r,c) not in visit and grid[r][c] == "1" :
-                    bfs(r,c)
-                    isalnds += 1
-                    
-        return isalnds
+            dfs(r+1,c)
+            dfs(r-1,c)
+            dfs(r,c+1)
+            dfs(r,c-1)
+            
+            return True
+        
+        count = 0
+        for row in range(rows) :
+            for column in range(columns) :
+                if dfs(row,column) :
+                    count += 1
+        
+        return count
+                
+            
+        
                     
                 
         
