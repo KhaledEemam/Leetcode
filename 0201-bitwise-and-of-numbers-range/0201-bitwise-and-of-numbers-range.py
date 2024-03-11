8ec80a29d3883781947f456ad936ecc591af4b92
KhaledEemam <khaledeemam@gmail.com>
2024-03-11 21:54:00 +0200

    Time: 56 ms (34.33%), Space: 16.6 MB (66.77%) - LeetHub

--- 0201-bitwise-and-of-numbers-range/0201-bitwise-and-of-numbers-range.py
+++ 0201-bitwise-and-of-numbers-range/0201-bitwise-and-of-numbers-range.py
@@ -1,10 +1,30 @@
 class Solution:
     def rangeBitwiseAnd(self, left: int, right: int) -> int:
-        i = 0
+        res = 0
+        
+        for i in range(32) :
+            l = (left >> i)  & 1
+            
+            if not l :
+                continue
+            
+            remain = left % (1 << (i+1))
+            diff = (1 << (i+1)) - remain
+            
+            if (right - left) < diff :
+                res = res | (l << i)
+                
+        return res
+            
+            
+            
+            
+            
+        """i = 0
         while left != right :
             left = left >> 1
             right = right >> 1
             i += 1
             
-        return left << i
+        return left << i"""
             
