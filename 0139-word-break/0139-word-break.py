35ca411f274dbac2f3a08e4843e8391ce7221d2a
KhaledEemam <khaledeemam@gmail.com>
2024-03-12 17:52:28 +0200

    Time: 35 ms (85.62%), Space: 16.8 MB (39.13%) - LeetHub

--- 0139-word-break/0139-word-break.py
+++ 0139-word-break/0139-word-break.py
@@ -1,6 +1,6 @@
 class Solution:
     def wordBreak(self, s: str, wordDict: List[str]) -> bool:
-        index = 0
+        """
         s_length = len(s)
         memo = {}
         
@@ -20,8 +20,18 @@ class Solution:
             memo[l] = False
             return False
         
-        
-        
         return get_word(0)
-            
+           """
+        dp = [False] * (len(s) + 1)
+        dp[-1] = True
+        
+        for i in range(len(s)-1,-1,-1) :
+            for w in wordDict :
+                if ((i+len(w)) <= len(s)) and s[i:i+len(w)] == w :
+                    dp[i] = dp[i+len(w)]
+                    
+                if dp[i] == True :
+                    break
+                    
+        return dp[0]
         
