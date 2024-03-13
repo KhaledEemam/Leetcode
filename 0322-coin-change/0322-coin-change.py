51f131b677921e1fc9c71346be641ff16d95fe40
KhaledEemam <khaledeemam@gmail.com>
2024-03-13 23:47:59 +0200

    Time: 783 ms (63.68%), Space: 17 MB (65.34%) - LeetHub

--- 0322-coin-change/0322-coin-change.py
+++ 0322-coin-change/0322-coin-change.py
@@ -1,7 +1,16 @@
 class Solution:
     def coinChange(self, coins: List[int], amount: int) -> int:
-        memo = {}
+        dp = [float("inf")] * (amount+1)
+        dp[0] = 0
+        for i in range(amount+1) :
+            for coin in coins :
+                if i-coin >= 0 :
+                    dp[i] = min(dp[i],(1 + dp[i-coin]))
+                
+        return dp[amount] if dp[amount] != float("inf") else -1
         
+        """
+        memo = {} 
         def get_steps(amt) :
             if amt == 0 : return 0
             if amt <= 0 : return float("inf")
@@ -19,3 +28,4 @@ class Solution:
         
         min_steps = get_steps(amount)
         return min_steps if min_steps != float("inf") else -1
+        """
