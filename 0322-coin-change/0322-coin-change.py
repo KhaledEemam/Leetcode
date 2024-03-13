44cd3b0ae1d11a38591f75fd7f48b7c52c120219
KhaledEemam <khaledeemam@gmail.com>
2024-03-13 17:43:44 +0200

    Time: 1126 ms (16.87%), Space: 45.3 MB (7.05%) - LeetHub

--- 0322-coin-change/0322-coin-change.py
+++ 0322-coin-change/0322-coin-change.py
@@ -1,28 +1,21 @@
 class Solution:
     def coinChange(self, coins: List[int], amount: int) -> int:
-        i = float("inf")
-        cur = 0
-        steps = 0
         memo = {}
-
-        def get_min_num(cur, steps):
-            nonlocal min_steps
-            if cur == amount:
-                return 0
-
-            if cur > amount:
-                return i
-
-            if cur in memo:
-                return memo[cur]
-
-            min_steps = i
-            for coin in coins:
-                min_steps = min(min_steps, 1 + get_min_num(cur + coin, steps + 1))
-
-            memo[cur] = min_steps
-
-            return min_steps
-
-        min_steps = get_min_num(cur, steps)
-        return min_steps if min_steps != i else -1
+        
+        def get_steps(amt) :
+            if amt == 0 : return 0
+            if amt <= 0 : return float("inf")
+            
+            if amt in memo : return memo[amt]
+            
+            steps = float("inf")
+            
+            for coin in coins :
+                steps = min(steps,1+get_steps(amt-coin))
+                
+            memo[amt] = steps
+            
+            return steps
+        
+        min_steps = get_steps(amount)
+        return min_steps if min_steps != float("inf") else -1
