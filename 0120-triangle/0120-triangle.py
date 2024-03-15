9aa215f1a0a1d89664105d05476fc8b07a352548
KhaledEemam <khaledeemam@gmail.com>
2024-03-15 17:44:57 +0200

    Time: 76 ms (5.98%), Space: 20 MB (12.30%) - LeetHub

--- 0120-triangle/0120-triangle.py
+++ 0120-triangle/0120-triangle.py
@@ -1,11 +1,30 @@
 class Solution:
     def minimumTotal(self, triangle: List[List[int]]) -> int:
-        dp = [0] * (len(triangle)+1)
         
+        memo = collections.defaultdict(list)
+        min_sum = float("inf")
+        ROWS = len(triangle)
+        
+        def dfs_sum(r,k):
+            if r == ROWS :
+                return 0
+            if (r,k) in memo :
+                return memo[(r,k)]
+            
+            cur_sum = triangle[r][k] + min(dfs_sum(r+1,k) , dfs_sum(r+1,k+1))
+            
+            memo[(r,k)] = cur_sum
+            return cur_sum
+        
+        return dfs_sum(0,0) 
+                
+        """
+        
+        dp = [0] * (len(triangle)+1)
         for row in triangle[::-1] :
             for i , n in enumerate(row) :
                 dp[i] = n + min(dp[i],dp[i+1])
                 
         return dp[0]
-            
+        """ 
         
