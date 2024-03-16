471370adc00403376afdde9533ede27d45eb93da
KhaledEemam <khaledeemam@gmail.com>
2024-03-16 22:07:02 +0200

    Time: 48 ms (27.70%), Space: 16.7 MB (58.89%) - LeetHub

--- 0097-interleaving-string/0097-interleaving-string.py
+++ 0097-interleaving-string/0097-interleaving-string.py
@@ -1,5 +1,24 @@
 class Solution:
     def isInterleave(self, s1: str, s2: str, s3: str) -> bool:
+        if len(s1) + len(s2) != len(s3) : return False
+        
+        dp = [[False] * (len(s1)+1) for i in range(len(s2)+1)]
+        dp[-1][-1] = True
+        
+        for r in range(len(dp)-1,-1,-1):
+            for c in range(len(dp[r])-1,-1,-1):
+                if c < len(s1) and s3[r+c] == s1[c] and dp[r][c+1] == True :
+                    dp[r][c] = True
+                if r < len(s2) and s3[r+c] == s2[r] and dp[r+1][c] == True :
+                    dp[r][c] = True
+                    
+        return dp[0][0]
+                
+        
+        
+        
+        """
+        # DFS
         memo = {}
         
         def dfs(l,r) :
@@ -22,5 +41,4 @@ class Solution:
             return False
         else :
             return dfs(0,0)
-        
-                
+            """
