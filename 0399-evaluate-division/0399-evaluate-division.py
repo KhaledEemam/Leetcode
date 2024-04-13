6f76faaddfa4b2db6db45d4693447db43eb4cd04
KhaledEemam <khaledeemam@gmail.com>
2024-04-13 17:09:02 +0200

    Time: 37 ms (53.02%), Space: 16.5 MB (99.46%) - LeetHub

--- 0399-evaluate-division/0399-evaluate-division.py
+++ 0399-evaluate-division/0399-evaluate-division.py
@@ -1,34 +1,35 @@
 class Solution:
     def calcEquation(self, equations: List[List[str]], values: List[float], queries: List[List[str]]) -> List[float]:
-        adj = collections.defaultdict(list)
-        
+        adj = defaultdict(list)
         for i , eq in enumerate(equations) :
             a , b = eq
             adj[a].append([b,values[i]])
-            adj[b].append([a, 1 / values[i] ])
-            
+            adj[b].append([a,1/values[i]])
             
         
-        def dfs(src,target) :
+        def bfs(src,target) :
             if src not in adj or target not in adj :
                 return -1
-            queue , visit = collections.deque() , set()
+            
+            queue , visit = deque() , set()
             queue.append((src,1))
             visit.add(src)
             
             while queue :
+                
                 s , w = queue.popleft()
-                if s == target :
-                    return w
-                for adjacent , weight in adj[s] :
-                    if adjacent not in visit :
-                        visit.add(adjacent)
-                        queue.append([adjacent,w*weight])
-            
+
+                for t , weight in adj[s] :
+                    
+                    if t == target :
+                        return w * weight
+                    
+                    if t not in visit :
+                        queue.append([t,w*weight])
+                        visit.add(t)
+                
             return -1
         
         
-        return [dfs(q[0],q[1]) for q in queries]
+        return [bfs(q[0],q[1]) for q in queries ]
             
-            
-        
