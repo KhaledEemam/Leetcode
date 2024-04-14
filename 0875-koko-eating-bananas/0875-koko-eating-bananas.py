4c0f83485d98c4f47131d18158dbedd1cfc2b47b
KhaledEemam <khaledeemam@gmail.com>
2024-04-14 16:06:06 +0200

    Time: 262 ms (72.21%), Space: 18.2 MB (57.77%) - LeetHub

--- 0875-koko-eating-bananas/0875-koko-eating-bananas.py
+++ 0875-koko-eating-bananas/0875-koko-eating-bananas.py
@@ -2,38 +2,30 @@ import math
 
 class Solution:
     def minEatingSpeed(self, piles: List[int], h: int) -> int:
-        def check_number(num) :
-            my_calc_hours = 0
+        
+        def check_speed(speed) :
+            hours = 0
             for pile in piles :
-                my_calc_hours += math.ceil(pile/num)
-
-            if my_calc_hours > h :
-                return -1 
-            else :
-                return num
-
-        def get_num(l,r) :
-            l = l
-            r = r
-            result = r
-            while r >= l :
-                m = (r+l) // 2
-                if check_number(m) == -1 :
-                    l = m + 1
-                else :
-                    result = min(result,check_number(m))
-                    r = m - 1
-
-            return result
-    
-
-        l = 1
-        r = max(piles)
-        max_possible_num =  get_num(l,r)
-
-        return max_possible_num
-            
+                hours += math.ceil(pile/speed)
                 
+            return hours
+        
+        
+        l , r = 1 , max(piles)
+        result = r
+        
+        while r >= l :
+            m = (l+r) // 2
             
+            cur_hours = check_speed(m)
+            
+            if cur_hours > h :
+                l = m + 1
+            else :
+                result = m
+                r = m - 1
+                
+        return result
+                
             
         
