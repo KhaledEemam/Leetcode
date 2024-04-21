5247de6d82fa6ea39c270f8fba780c9d2404ba4b
KhaledEemam <khaledeemam@gmail.com>
2024-04-21 19:08:12 +0200

    Time: 40 ms (25.23%), Space: 16.6 MB (41.60%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.py
+++ 0062-unique-paths/0062-unique-paths.py
@@ -1,5 +1,19 @@
 class Solution:
-    def uniquePaths(self, m: int, n: int) -> int:   
+    def uniquePaths(self, m: int, n: int) -> int:
+        # dp
+        dp = [[0] * (n+1) for i in range(m+1) ]
+        ROWS , COLUMNS = len(dp) , len(dp[0])
+        dp[ROWS-2][COLUMNS-1] = 1
+        
+        for i in range(ROWS-2,-1,-1) :
+            for k in range(COLUMNS-2,-1,-1) :
+                dp[i][k] = dp[i+1][k] + dp[i][k+1]
+                
+        return dp[0][0]
+        
+        
+        
+        """
         ROWS , COLUMNS = m , n
         paths = 0
         memo = {}
@@ -15,14 +29,11 @@ class Solution:
             if (x,y) in memo :
                 return memo[(x,y)]
             
-            #visited.add((x,y))
             
             paths = dfs(x+1,y) + dfs(x,y+1)
-            
-            #visited.remove((x,y))
             memo[(x,y)] = paths
             
             return paths
         
         return dfs(0,0)
-        
+        """
