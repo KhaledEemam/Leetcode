c9ef66ebd01bc71cdeb772b4b743a38274c2475a
KhaledEemam <khaledeemam@gmail.com>
2024-04-21 18:53:22 +0200

    Time: 41 ms (20.13%), Space: 17 MB (6.39%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.py
+++ 0062-unique-paths/0062-unique-paths.py
@@ -1,33 +1,29 @@
 class Solution:
     def uniquePaths(self, m: int, n: int) -> int:
-        """
-        cache = [[0]*n for i in range(m)]
+        ROWS , COLUMNS = m , n
+        paths = 0
+        memo = {}
+        visited = set()
         
-        def move(x,y,m,n) :
-            if x > m-1 or y > n-1 :
+        def dfs(x,y) :
+            if x < 0 or x > ROWS-1 or y < 0 or y > COLUMNS-1 or (x,y) in visited :
                 return 0
-            if x == m-1 and y == n-1 :
+
+            if x == ROWS-1 and y == COLUMNS -1 :
                 return 1
-            if cache[x][y] > 0 :
-                return cache[x][y]
             
-            cache[x][y] = move(x+1,y,m,n) + move(x,y+1,m,n)
-            return cache[x][y]
+            if (x,y) in memo :
+                return memo[(x,y)]
+            
+            visited.add((x,y))
+            
+            paths = dfs(x+1,y) + dfs(x,y+1)
+            
+            visited.remove((x,y))
+            memo[(x,y)] = paths
+            
+            return paths
         
-        return move(0,0,m,n)
-           """
-        prevrow = [0] * n
-        
-        for i in range(m-1 , -1 , -1):
-            currow = [0] * n
-            currow[n-1] = 1
-            for k in range(n-2 , -1 , -1) :
-                currow[k] = currow[k+1] + prevrow[k]
-            
-            prevrow = currow
-            
-        return prevrow[0]
+        return dfs(0,0)
                 
-            
-            
-            
+        
