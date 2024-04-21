f20a249f35351018280223c956fd5b13d8f3b293
KhaledEemam <khaledeemam@gmail.com>
2024-04-21 18:54:53 +0200

    Time: 39 ms (31.52%), Space: 16.8 MB (6.39%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.py
+++ 0062-unique-paths/0062-unique-paths.py
@@ -1,12 +1,12 @@
 class Solution:
-    def uniquePaths(self, m: int, n: int) -> int:
+    def uniquePaths(self, m: int, n: int) -> int:   
         ROWS , COLUMNS = m , n
         paths = 0
         memo = {}
         visited = set()
         
         def dfs(x,y) :
-            if x < 0 or x > ROWS-1 or y < 0 or y > COLUMNS-1 or (x,y) in visited :
+            if x < 0 or x > ROWS-1 or y < 0 or y > COLUMNS-1  :
                 return 0
 
             if x == ROWS-1 and y == COLUMNS -1 :
@@ -15,15 +15,14 @@ class Solution:
             if (x,y) in memo :
                 return memo[(x,y)]
             
-            visited.add((x,y))
+            #visited.add((x,y))
             
             paths = dfs(x+1,y) + dfs(x,y+1)
             
-            visited.remove((x,y))
+            #visited.remove((x,y))
             memo[(x,y)] = paths
             
             return paths
         
         return dfs(0,0)
-                
         
